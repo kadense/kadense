367bb7fc53be79f7a0edf4f663c3374a5139073a
shaun.turner-hitcs <shaun.turner@headinthecloudsolutions.co.uk>
2025-04-27 15:58:12 +0000

    Fix to metadata

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Kadense is a framework for building applications and services on Kubernetes.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

11fb7ccd7f211d4d2f412872f1b65146bd31e035
shaun turner <shaun.turner1@nhs.net>
2025-05-29 12:25:27 +0000

    updated docs to use kadense.io instead of kadense.github.io

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   ],
 
   // Set the production url of your site here
-  url: 'https://kadense.github.io/',
+  url: 'https://kadense.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kadense/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
